2e1dbc885896c829586206b9f466571039da9cf0
Sebastian Valenzuela <seba123612@gmail.com>
2023-11-28 01:09:40 -0300

    Added clickable icons to navbar

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,29 @@ import { useState } from "react";
 
 function App() {
   const [area, setArea] = useState("0");
+  const [page, setPage] = useState("map")
+
+  const setPeople = () => {
+    setPage("people")
+  }
+
+  const setMap = () => {
+    setPage("map")
+  }
+
+  const setChart = () => {
+    setPage("chart")
+  }
+
   const onClick = (e) => {
     setArea(e.point.id)
   }
 
   return ( 
   <div className="container">
-    <NavBar/>
+    <NavBar setChart={setChart} setMap={setMap} setPeople={setPeople}/>
     <Map onClick={onClick}/>
-    <h1>Area: {area}</h1>
+    <h1>Area: {area}, Page: {page}</h1>
   </div>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,22 @@
 import "./Navbar.css"
+import { FaMapLocationDot } from "react-icons/fa6";
+import { FaChartBar } from "react-icons/fa";
+import { BsPeopleFill } from "react-icons/bs";
 
 
-const NavBar = () => {
+interface NavBarProps {
+    setPeople: () => void;
+    setMap: () => void;
+    setChart: () => void;
+}
+
+const NavBar = ({setPeople, setMap, setChart}: NavBarProps) => {
     return (
-        <div className="navbar-container"></div>
+        <div className="navbar-container">
+        <FaMapLocationDot size="45" className="navbar-icon" onClick={setMap}/>
+        <FaChartBar size="45" className = "navbar-icon" onClick={setChart}/>
+        <BsPeopleFill size="45" className = "navbar-icon" onClick={setPeople}/>
+        </div>
     );
 };
 
