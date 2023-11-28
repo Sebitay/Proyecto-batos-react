9c28d478c4395340a8847c9da27f0ba70b22c3ec
Sebastian Valenzuela <seba123612@gmail.com>
2023-11-28 00:25:29 -0300

    Added general locations and working map

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
-import Map from "./Map";
+import Map from "./components/Map";
+import NavBar from "./components/Navbar";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
-  return <Map />;
+  const [area, setArea] = useState("0");
+  const onClick = (e) => {
+    setArea(e.point.id)
+  }
+
+  return ( 
+  <div className="container">
+    <NavBar/>
+    <Map onClick={onClick}/>
+    <h1>Area: {area}</h1>
+  </div>
+  );
 }
 
 export default App;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,10 @@
+import "./Navbar.css"
+
+
+const NavBar = () => {
+    return (
+        <div className="navbar-container"></div>
+    );
+};
+
+export default NavBar;
