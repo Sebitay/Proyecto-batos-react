1bacf24c39dd3c0a7b9058273b638399d143ddf1
Sebastian Valenzuela <seba123612@gmail.com>
2023-11-29 03:14:35 -0300

    Added functionality to heat map

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 import database as db
 
@@ -14,6 +14,15 @@ def message(id):
             'nCrimes': data[int(id)-1][1]
         }
     return response
+
+@app.route('/infoMap', methods=['GET'])
+def infoMap():
+    if request.method == 'GET':
+        data = db.nCrimenesArea()
+        response = []
+        for item in data:
+            response.append(item[1])
+    return jsonify(response)
 
 
 if __name__ == "__main__":
